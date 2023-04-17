e5791f5e4bb89eabeca265b88c1eb5a80e25ccda
mosafari <m.safari2680@gmail.com>
2023-04-17 11:58:30 +0430

    create project file with Spring initializer

---
+++ src/test/java/com/webapp/Survey/SurveyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webapp.Survey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurveyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
