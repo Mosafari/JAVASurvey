2b366829280c44840489cc232767e2b4c9d30aef
mosafari <m.safari2680@gmail.com>
2023-04-22 22:11:27 +0430

    show all comments into a table in comments page

--- temp/src/main/java/com/webapp/Survey/Addpage.java
+++ temp/src/main/java/com/webapp/Survey/Addpage.java
@@ -63,9 +63,7 @@ public class Addpage {
             		+ "            </div>\r\n"
             		+ "        </div>\r\n"
             		+ "    </section>\r\n"
-            		+ "    <div id=\"SCRtepm\"></div>\r\n"
             		+ "</body>\r\n"
-            		+ "\r\n"
             		+ "</html>";
         return str;
     }
@@ -119,9 +117,7 @@ public class Addpage {
         		+ "            </div>\r\n"
         		+ "        </div>\r\n"
         		+ "    </section>\r\n"
-        		+ "    <div id=\"SCRtepm\"></div>\r\n"
         		+ "</body>\r\n"
-        		+ "\r\n"
         		+ "</html>";
     return str;
         }
@@ -156,7 +152,69 @@ public class Addpage {
     )
     public String commentspage(String args[]) throws InterruptedException 
     	{
-            return "Comments Page";
+    	String result = "";
+    	try {
+	    	Connection con=DriverManager.getConnection(  
+	        		"jdbc:mysql://localhost:3306/javasql","root","test");  
+	        		//here javasql is database name, root is username and password is test
+	    	Statement stmt=con.createStatement();  
+	    	ResultSet rs=stmt.executeQuery("select * from Comments");  
+	    	while(rs.next())
+	    		result += "      <div class=\"columns\">\r\n"
+        		+ "        <div class=\"column\">"+rs.getInt(1)+"</div>\r\n"
+        		+ "        <div class=\"column\">"+rs.getString(4)+"</div>\r\n"
+        		+ "        <div class=\"column\">"+rs.getString(3)+"</div>\r\n"
+        		+ "        <div class=\"column\">"+rs.getString(2)+"</div>\r\n"
+        		+ "      </div>\r\n";
+	    	
+	    	con.close();  
+    	}catch(Exception e){ System.out.println(e);}  
+    	String str
+        = "<!DOCTYPE html>\r\n"
+        		+ "<html>\r\n"
+        		+ "\r\n"
+        		+ "<head>\r\n"
+        		+ "    <meta charset=\"utf-8\">\r\n"
+        		+ "    <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">\r\n"
+        		+ "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\r\n"
+        		+ "    <title>Home</title>\r\n"
+        		+ "    <link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.2/css/bulma.min.css\" />\r\n"
+        		+ "    <script src=\"https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js\"></script>\r\n"
+        		+ "    <script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js\"></script>\r\n"
+        		+ "    <script src=\"https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js\" integrity=\"sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN\" crossorigin=\"anonymous\"></script>\r\n"
+        		+ "</head>\r\n"
+        		+ "\r\n"
+        		+ "<body>\r\n"
+        		+ "    <section class=\"hero is-primary is-fullheight\">\r\n"
+        		+ "\r\n"
+        		+ "        <div class=\"hero-head\">\r\n"
+        		+ "        </div>\r\n"
+        		+ "\r\n"
+        		+ "        <div class=\"hero-body\">\r\n"
+        		+ "            <div class=\"container has-text-centered\" >\r\n"
+        		+ "					<div class=\"column is-4 is-offset-4\" >"				
+        		+"						<div class=\"box\">"
+        		+"							<div class=\"field\">\r\n"
+        		+"          					<strong>Comments</strong>\r\n"
+        		+ "    						</div>"
+        		+"						</div>"
+        		+"						<div class=\"box\">\r\n"
+        		+ "    						<div class=\"field\">\r\n"
+        		+ "     						 <div class=\"columns\">\r\n"
+        		+ "       						 <div class=\"column\">ID</div>\r\n"
+        		+ "       						 <div class=\"column\">Name</div>\r\n"
+        		+ "       						 <div class=\"column\">Comment</div>\r\n"
+        		+ "       						 <div class=\"column\">Date</div>\r\n"
+        		+ "      					</div>\r\n"
+        		+ 							result
+        		+ "    				</div>\r\n"
+        		+ "            </div>\r\n"
+        		+ "        </div>\r\n"
+        		+ "    </section>\r\n"
+        		+ "</body>\r\n"
+        		+ "</html>";
+    	
+            return str;
         }
 }
 
